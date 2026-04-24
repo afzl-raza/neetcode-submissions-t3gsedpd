ecc6597ecd1d11bc41335e901b9417c44d27a88a
afzl-raza <afzaldis2@gmail.com>
2026-04-24 21:23:03 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        boolean[][] dp = new boolean[n][n];
+        int count=0;
+        for (int i=0;i<n;i++){
+            dp[i][i] = true;
+            count++;
+        }
+        for (int len=2;len<=n;len++){
+            for (int i=0;i<n-len+1;i++){
+                int j =i+len-1;
+                if(s.charAt(i)!=s.charAt(j)){
+                    dp[i][j]=false;
+                }
+                else{
+                    if(len==2||dp[i+1][j-1]){
+                        dp[i][j] = true;
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;      
+    }
+}
+
