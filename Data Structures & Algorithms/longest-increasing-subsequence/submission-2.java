ebe0109204b55a7bae018fb957a841febf4cdfdf
afzl-raza <afzaldis2@gmail.com>
2026-04-25 15:57:52 +0530

    Add: longest-increasing-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int lenOfLIS(int[]nums,int i,int[]dp){
+        if(i<0){
+            return 0;
+        }
+        if(dp[i]!=1){
+            return dp[i];
+        }
+        int max = 1;
+        for(int j=i;j>=0;j--){
+            if(nums[i]>nums[j]){
+                int curr = 1+lenOfLIS(nums,j,dp);
+                max = Math.max(curr,max);
+            }
+        }
+        return dp[i]=max;
+    }
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,1); // because every elm is a single subseq.
+        int ans =1;
+        for(int i=nums.length-1;i>=0;i--){
+            int curr=lenOfLIS(nums,i,dp);
+            ans = Math.max(ans,curr);
+
+        }
+        return (ans);    
+    }
+}
