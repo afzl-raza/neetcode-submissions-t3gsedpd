4c3a8fc4506f45cb197c224b6202637e3ce91973
afzl-raza <afzaldis2@gmail.com>
2026-08-18 22:43:48 +0530

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean checkJump(int[] nums, int indx,int[] canReach){
+        if(canReach[indx]!=-1) return canReach[indx]==1;
+        if(indx<nums.length-1 && nums[indx]==0){
+            canReach[indx] = 0;
+            return false;
+        }
+        if(indx==nums.length-1) {
+            canReach[indx] = 1;
+            return true;
+        }
+        for(int i=indx+1;i<=Math.min(indx+nums[indx],nums.length-1);i++){
+            if(checkJump(nums,i,canReach)) {
+                canReach[indx] =1;
+                return true;
+            }
+        }
+        canReach[indx] =0;
+        return false;
+    }
+    public boolean canJump(int[] nums) {
+        int[] canReach = new int[nums.length];
+        Arrays.fill(canReach,-1);
+        return checkJump(nums,0,canReach);    
+    }
+    
+}
